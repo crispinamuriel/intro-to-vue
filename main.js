815cc6232a1fbeeb615db5d0ebd0dc7861ccbaf2
crispinamuriel <crispinamuriel@gmail.com>
2020-04-17 03:23:23 -0400

    conditional vue directives

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+/* eslint-disable quotes */
 console.log('working as intended');
 
 const app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    product: 'Socks'
-  }
+    product: "Socks",
+    description: "These are comfy green socks! Made with 100% cotton.",
+    image:
+      "https://media.mysockfactory.ch/1313-thickbox_default/plain-green-socks.jpg",
+    link: "https://www.happysocks.com/us/jumbo-dot-sock-green.html",
+    inStock: true,
+    inventory: 1,
+    onSale: true,
+  },
 });
