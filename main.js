8958d8ed5913838f60ba16e033ff8ea8eebb20e2
crispinamuriel <crispinamuriel@gmail.com>
2020-04-19 20:00:21 -0400

    v-on mouseover

--- main.js
+++ main.js
@@ -9,21 +9,40 @@ const app = new Vue({
     image:
       "https://media.mysockfactory.ch/1313-thickbox_default/plain-green-socks.jpg",
     link: "https://www.happysocks.com/us/jumbo-dot-sock-green.html",
-    inStock: true,
-    inventory: 1,
+    inStock: false,
+    inventory: 0,
     onSale: true,
     details: ["100% cotton", "size 8-12", "men's socks"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
+        variantImage:
+          "https://media.mysockfactory.ch/1313-thickbox_default/plain-green-socks.jpg",
       },
       {
         variantId: 2235,
         variantColor: "blue",
+        variantImage:
+          "https://media.mysockfactory.ch/1428-thickbox_default/spy-blue-plain-socks.jpg",
       },
     ],
     sizes: ["size 4-7", "size 8-12", "size 13-15"],
-    cart: 0
-  }
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    resetCart() {
+      this.cart = 0;
+    },
+    updateProduct(img) {
+      this.image = img;
+    },
+    decrementCart() {
+      if (this.cart < 1) return;
+      this.cart -= 1;
+    }
+  },
 });
