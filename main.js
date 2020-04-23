f10e63b49e836745a504f342bcd34cd550804074
crispinamuriel <crispinamuriel@gmail.com>
2020-04-22 22:13:45 -0400

    computed values

--- main.js
+++ main.js
@@ -4,31 +4,38 @@ console.log('working as intended');
 const app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue Mastery",
+    outOfStock: "outOfStock",
+    lineThrough: "lineThrough",
     product: "Socks",
     description: "These are comfy green socks! Made with 100% cotton.",
-    image:
-      "https://media.mysockfactory.ch/1313-thickbox_default/plain-green-socks.jpg",
+    selectedVariant: 0,
     link: "https://www.happysocks.com/us/jumbo-dot-sock-green.html",
-    inStock: false,
     inventory: 0,
     onSale: true,
-    details: ["100% cotton", "size 8-12", "men's socks"],
+    details: ["100% cotton", "size 4-15", "Men's socks"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
         variantImage:
           "https://media.mysockfactory.ch/1313-thickbox_default/plain-green-socks.jpg",
+        variantQuantity: 10,
       },
       {
         variantId: 2235,
-        variantColor: "blue",
+        variantColor: "cornflowerblue",
         variantImage:
           "https://media.mysockfactory.ch/1428-thickbox_default/spy-blue-plain-socks.jpg",
+        variantQuantity: 0,
       },
     ],
     sizes: ["size 4-7", "size 8-12", "size 13-15"],
     cart: 0,
+    styles: {
+      color: "red",
+      "text-decoration-line": "line-through",
+    },
   },
   methods: {
     addToCart() {
@@ -37,12 +44,23 @@ const app = new Vue({
     resetCart() {
       this.cart = 0;
     },
-    updateProduct(img) {
-      this.image = img;
+    updateProduct(i) {
+      this.selectedVariant = i;
     },
     decrementCart() {
       if (this.cart < 1) return;
       this.cart -= 1;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity > 0;
     }
   },
 });
